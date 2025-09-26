ce70065c12d01ff5ef77d7429bf7a8c0e82f4122
Drunken-Crabapple <3848934203@qq.com>
2025-09-26 19:22:32 +0800

    V1.4

--- balance car V1.3/Src/Timer.c
+++ balance car V1.3/Src/Timer.c
@@ -110,10 +110,13 @@ void TImer_IT_init_ms(uint32_t ms){
 	TIM_TimeBaseInitStructure.TIM_RepetitionCounter = 0;
 	TIM_TimeBaseInit(TIM1, &TIM_TimeBaseInitStructure);
 	
+	TIM_ClearFlag(TIM1,TIM_FLAG_Update);
 	TIM_ITConfig(TIM1,TIM_IT_Update,ENABLE);
 	
-	NVIC_InitTypeDef NVIC_InitStructure;
 	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);//Set NVIC
+	
+	NVIC_InitTypeDef NVIC_InitStructure;
+	NVIC_InitStructure.NVIC_IRQChannel = TIM1_UP_IRQn;
 	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
 	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 2;
 	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
